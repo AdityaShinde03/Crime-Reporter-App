55613cb246b567983b78a27937b30a61b1823476
Ved Panchal <vedpanchal99@gmail.com>
2024-06-29 16:10:27 +0530

    Configure frontend

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,4 +1,8 @@
 import { useState } from 'react'
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import HomePage from './pages/HomePage';
+import Header from './components/header/Header';
+import Footer from './components/footer/Footer';
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
@@ -8,26 +12,14 @@ function App() {
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          
+        </Routes>
+        <Footer />
+      </BrowserRouter>
     </>
   )
 }
---
+++ Frontend/src/components/home/Home.jsx
@@ -0,0 +1,17 @@
+import { useEffect, useState } from "react";
+import config from "../../services/config";
+
+const Home = () => {
+  
+
+  return (
+   <>
+
+    <h1>
+      Hello
+    </h1>
+   </>
+  );
+};
+
+export default Home;
