6ff0a60e992143da3daeaca5f5caf7c803bfe3ac
Ved Panchal <vedpanchal99@gmail.com>
2024-06-29 10:26:28 +0530

    Configure a temp store and reducer

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { NextUIProvider } from "@nextui-org/react";
 import { Provider } from "react-redux";
+import { store } from "./services/redux/store" 
 import App from './App.jsx'
 import './index.css'
 
---
+++ Frontend/src/services/redux/rootReducer.js
@@ -0,0 +1,10 @@
+// src/services/redux/reducers/index.js
+import { combineReducers } from 'redux';
+import authReducers from './reducers/authReducers';
+
+const rootReducer = combineReducers({
+  auth: authReducers,
+
+});
+
+export default rootReducer;
